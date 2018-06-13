153fc9e17483e1d57b5ffed05a23aa552b0eba54
Marvin Witt <marvin.witt@elementies.com>
2018-06-13 20:32:55 +0200

    Adding EventOnSpellHit and EventOnMoveSuccess to the working events

---
+++ GameServerLib/Logic/GameObjects/AttackableUnits/AttackableUnit.cs
@@ -0,0 +1,313 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using LeagueSandbox.GameServer.Logic.API;
+using LeagueSandbox.GameServer.Logic.Content;
+using LeagueSandbox.GameServer.Logic.Items;
+
+namespace LeagueSandbox.GameServer.Logic.GameObjects.AttackableUnits
+{
+    public class AttackableUnit : GameObject
+    {
+        internal const float DETECT_RANGE = 475.0f;
+        internal const int EXP_RANGE = 1400;
+
+        protected Stats Stats { get; }
+        private float _statUpdateTimer;
+        public bool IsModelUpdated { get; set; }
+        public bool IsDead { get; protected set; }
+        private string _model;
+        public string Model
+        {
+            get { return _model; }
+            set
+            {
+                _model = value;
+                IsModelUpdated = true;
+            }
+        }
+        protected Logger _logger = Program.ResolveDependency<Logger>();
+        public InventoryManager Inventory { get; protected set; }
+        public int KillDeathCounter { get; protected set; }
+
+        public AttackableUnit(
+            string model,
+            Stats stats,
+            int collisionRadius = 40,
+            float x = 0,
+            float y = 0,
+            int visionRadius = 0,
+            uint netId = 0
+        ) : base(x, y, collisionRadius, visionRadius, netId)
+
+        {
+            Stats = stats;
+            Model = model;
+            CollisionRadius = 40;
+            Stats.AttackSpeedMultiplier.BaseValue = 1.0f;
+        }
+
+        public override void OnAdded()
+        {
+            base.OnAdded();
+            _game.ObjectManager.AddVisionUnit(this);
+        }
+
+        public override void OnRemoved()
+        {
+            base.OnRemoved();
+            _game.ObjectManager.RemoveVisionUnit(this);
+        }
+
+        public Stats GetStats()
+        {
+            return Stats;
+        }
+
+        public override void update(float diff)
+        {
+            base.update(diff);
+
+            _statUpdateTimer += diff;
+            while (_statUpdateTimer >= 500)
+            { // update Stats (hpregen, manaregen) every 0.5 seconds
+                Stats.update(_statUpdateTimer);
+                _statUpdateTimer -= 500;
+            }
+        }
+
+        public override float getMoveSpeed()
+        {
+            return Stats.MoveSpeed.Total;
+        }
+
+        public virtual void die(AttackableUnit killer)
+        {
+            setToRemove();
+            _game.ObjectManager.StopTargeting(this);
+
+            _game.PacketNotifier.NotifyNpcDie(this, killer);
+
+            var exp = _game.Map.MapGameScript.GetExperienceFor(this);
+            var champs = _game.ObjectManager.GetChampionsInRange(this, EXP_RANGE, true);
+            //Cull allied champions
+            champs.RemoveAll(l => l.Team == Team);
+
+            if (champs.Count > 0)
+            {
+                float expPerChamp = exp / champs.Count;
+                foreach (var c in champs)
+                {
+                    c.GetStats().Experience += expPerChamp;
+                    _game.PacketNotifier.NotifyAddXP(c, expPerChamp);
+                }
+            }
+
+            if (killer != null)
+            {
+                var cKiller = killer as Champion;
+
+                if (cKiller == null)
+                    return;
+
+                var gold = _game.Map.MapGameScript.GetGoldFor(this);
+                if (gold <= 0)
+                {
+                    return;
+                }
+
+                cKiller.GetStats().Gold += gold;
+                _game.PacketNotifier.NotifyAddGold(cKiller, this, gold);
+
+                if (cKiller.KillDeathCounter < 0)
+                {
+                    cKiller.ChampionGoldFromMinions += gold;
+                    _logger.LogCoreInfo($"Adding gold form minions to reduce death spree: {cKiller.ChampionGoldFromMinions}");
+                }
+
+                if (cKiller.ChampionGoldFromMinions >= 50 && cKiller.KillDeathCounter < 0)
+                {
+                    cKiller.ChampionGoldFromMinions = 0;
+                    cKiller.KillDeathCounter += 1;
+                }
+            }
+
+            if (IsDashing)
+            {
+                IsDashing = false;
+            }
+        }
+
+        public virtual bool isInDistress()
+        {
+            return false; //return DistressCause;
+        }
+
+        public virtual void TakeDamage(AttackableUnit attacker, float damage, DamageType type, DamageSource source,
+            DamageText damageText)
+        {
+            float defense = 0;
+            float regain = 0;
+            var attackerStats = attacker.GetStats();
+
+            switch (type)
+            {
+                case DamageType.DAMAGE_TYPE_PHYSICAL:
+                    defense = GetStats().Armor.Total;
+                    defense = (1 - attackerStats.ArmorPenetration.PercentBonus) * defense -
+                              attackerStats.ArmorPenetration.FlatBonus;
+
+                    break;
+                case DamageType.DAMAGE_TYPE_MAGICAL:
+                    defense = GetStats().MagicPenetration.Total;
+                    defense = (1 - attackerStats.MagicPenetration.PercentBonus) * defense -
+                              attackerStats.MagicPenetration.FlatBonus;
+                    break;
+                case DamageType.DAMAGE_TYPE_TRUE:
+                    break;
+                default:
+                    throw new ArgumentOutOfRangeException(nameof(type), type, null);
+            }
+
+            switch (source)
+            {
+                case DamageSource.DAMAGE_SOURCE_SPELL:
+                    regain = attackerStats.SpellVamp.Total;
+                    break;
+                case DamageSource.DAMAGE_SOURCE_ATTACK:
+                    regain = attackerStats.LifeSteal.Total;
+                    break;
+                case DamageSource.DAMAGE_SOURCE_SUMMONER_SPELL:
+                    break;
+                case DamageSource.DAMAGE_SOURCE_PASSIVE:
+                    break;
+                default:
+                    throw new ArgumentOutOfRangeException(nameof(source), source, null);
+            }
+
+            //Damage dealing. (based on leagueoflegends' wikia)
+            damage = defense >= 0 ? (100 / (100 + defense)) * damage : (2 - (100 / (100 - defense))) * damage;
+
+            ApiEventManager.OnUnitDamageTaken.Publish(this);
+
+
+            GetStats().CurrentHealth = Math.Max(0.0f, GetStats().CurrentHealth - damage);
+            if (!IsDead && GetStats().CurrentHealth <= 0)
+            {
+                IsDead = true;
+                die(attacker);
+            }
+
+            _game.PacketNotifier.NotifyDamageDone(attacker, this, damage, type, damageText);
+            _game.PacketNotifier.NotifyUpdatedStats(this, false);
+
+            // Get health from lifesteal/spellvamp
+            if (regain > 0)
+            {
+                attackerStats.CurrentHealth = Math.Min(attackerStats.HealthPoints.Total,
+                    attackerStats.CurrentHealth + regain * damage);
+                _game.PacketNotifier.NotifyUpdatedStats(attacker, false);
+            }
+        }
+
+        public virtual void TakeDamage(AttackableUnit attacker, float damage, DamageType type, DamageSource source, bool isCrit)
+        {
+            var text = DamageText.DAMAGE_TEXT_NORMAL;
+
+            if (isCrit)
+            {
+                text = DamageText.DAMAGE_TEXT_CRITICAL;
+            }
+
+            TakeDamage(attacker, damage, type, source, text);
+        }
+
+        public virtual void TakeDamageBySpell(AttackableUnit attacker, float damage, DamageType type, DamageSource source, bool isCrit, Spell spell)
+        {
+            var text = DamageText.DAMAGE_TEXT_NORMAL;
+
+            if (isCrit)
+            {
+                text = DamageText.DAMAGE_TEXT_CRITICAL;
+            }
+
+            ApiEventManager.OnSpellHit.Publish(this, spell);
+
+            TakeDamage(attacker, damage, type, source, text);
+        }
+    }
+
+    public enum UnitAnnounces : byte
+    {
+        Death = 0x04,
+        InhibitorDestroyed = 0x1F,
+        InhibitorAboutToSpawn = 0x20,
+        InhibitorSpawned = 0x21,
+        TurretDestroyed = 0x24,
+        SummonerDisconnected = 0x47,
+        SummonerReconnected = 0x48
+    }
+
+    public enum ClassifyUnit
+    {
+        ChampionAttackingChampion = 1,
+        MinionAttackingChampion = 2,
+        MinionAttackingMinion = 3,
+        TurretAttackingMinion = 4,
+        ChampionAttackingMinion = 5,
+        Placeable = 6,
+        MeleeMinion = 7,
+        CasterMinion = 8,
+        SuperOrCannonMinion = 9,
+        Turret = 10,
+        Champion = 11,
+        Inhibitor = 12,
+        Nexus = 13,
+        Default = 14
+    }
+
+    public enum DamageType : byte
+    {
+        DAMAGE_TYPE_PHYSICAL = 0x0,
+        DAMAGE_TYPE_MAGICAL = 0x1,
+        DAMAGE_TYPE_TRUE = 0x2
+    }
+
+    public enum DamageText : byte
+    {
+        DAMAGE_TEXT_INVULNERABLE = 0x0,
+        DAMAGE_TEXT_DODGE = 0x2,
+        DAMAGE_TEXT_CRITICAL = 0x3,
+        DAMAGE_TEXT_NORMAL = 0x4,
+        DAMAGE_TEXT_MISS = 0x5
+    }
+
+    public enum DamageSource
+    {
+        DAMAGE_SOURCE_ATTACK,
+        DAMAGE_SOURCE_SPELL,
+        DAMAGE_SOURCE_SUMMONER_SPELL, // Ignite shouldn't destroy Banshee's
+        DAMAGE_SOURCE_PASSIVE // Red/Thornmail shouldn't as well
+    }
+
+    public enum AttackType : byte
+    {
+        ATTACK_TYPE_RADIAL,
+        ATTACK_TYPE_MELEE,
+        ATTACK_TYPE_TARGETED
+    }
+
+    public enum MoveOrder
+    {
+        MOVE_ORDER_MOVE,
+        MOVE_ORDER_ATTACKMOVE
+    }
+
+    public enum ShieldType : byte
+    {
+        GreenShield = 0x01,
+        MagicShield = 0x02,
+        NormalShield = 0x03
+    }
+}
---
+++ GameServerLib/Logic/GameObjects/Spells/Spell.cs
@@ -0,0 +1,366 @@
+﻿using LeagueSandbox.GameServer.Core.Logic;
+using System;
+using System.Collections.Generic;
+using LeagueSandbox.GameServer.Logic.API;
+using LeagueSandbox.GameServer.Logic.Scripting.CSharp;
+using Newtonsoft.Json.Linq;
+using LeagueSandbox.GameServer.Logic.Content;
+using LeagueSandbox.GameServer.Logic.GameObjects.AttackableUnits;
+using LeagueSandbox.GameServer.Logic.Packets;
+using LeagueSandbox.GameServer.Logic.Packets.PacketDefinitions.S2C;
+using LeagueSandbox.GameServer.Logic.Packets.PacketHandlers;
+
+namespace LeagueSandbox.GameServer.Logic.GameObjects
+{
+    public enum SpellState
+    {
+        STATE_READY,
+        STATE_CASTING,
+        STATE_COOLDOWN,
+        STATE_CHANNELING
+    };
+
+    public class Spell
+    {
+        public static bool CooldownsEnabled;
+        public static bool ManaCostsEnabled;
+        public Champion Owner { get; private set; }
+        public short Level { get; private set; }
+        public byte Slot { get; set; }
+        public float CastTime { get; private set; } = 0;
+
+        public string SpellName { get; private set; }
+        public bool HasEmptyScript { get { return spellGameScript.GetType() == typeof(GameScriptEmpty); } }
+
+        public SpellState state { get; protected set; } = SpellState.STATE_READY;
+        public float CurrentCooldown { get; protected set; }
+        public float CurrentCastTime { get; protected set; }
+        public float CurrentChannelDuration { get; protected set; }
+        public uint FutureProjNetId { get; protected set; }
+        public uint SpellNetId { get; protected set; }
+
+        public AttackableUnit Target { get; private set; }
+        public float X { get; private set; }
+        public float Y { get; private set; }
+        public float X2 { get; private set; }
+        public float Y2 { get; private set; }
+
+        private static CSharpScriptEngine _scriptEngine = Program.ResolveDependency<CSharpScriptEngine>();
+        private static Logger _logger = Program.ResolveDependency<Logger>();
+        private static Game _game = Program.ResolveDependency<Game>();
+
+        private GameScript spellGameScript;
+        protected NetworkIdManager _networkIdManager = Program.ResolveDependency<NetworkIdManager>();
+
+        public SpellData SpellData { get; private set; }
+
+        static Spell()
+        {
+            CooldownsEnabled = _game.Config.CooldownsEnabled;
+            ManaCostsEnabled = _game.Config.ManaCostsEnabled;
+        }
+
+        public Spell(Champion owner, string spellName, byte slot)
+        {
+            Owner = owner;
+            SpellName = spellName;
+            Slot = slot;
+            SpellData = _game.Config.ContentManager.GetSpellData(spellName);
+            _scriptEngine = Program.ResolveDependency<CSharpScriptEngine>();
+
+            //Set the game script for the spell
+            if (spellName != "")
+            {
+                spellGameScript = _scriptEngine.CreateObject<GameScript>("Spells", spellName);
+            }
+            if (spellGameScript == null)
+            {
+                spellGameScript = new GameScriptEmpty();
+            }
+            //Activate spell - Notes: Deactivate is never called as spell removal hasn't been added
+            spellGameScript.OnActivate(owner);
+        }
+
+        // TODO: Make a better way to deactivate spells; this is placeholder for activated items.
+        public void DeactivateSpell()
+        {
+            spellGameScript.OnDeactivate(Owner);
+        }
+
+        /// <summary>
+        /// Called when the character casts the spell
+        /// </summary>
+        public virtual bool cast(float x, float y, float x2, float y2, AttackableUnit u = null)
+        {
+            if (HasEmptyScript) return false;
+
+            var stats = Owner.GetStats();
+            if ((SpellData.ManaCost[Level] * (1 - stats.getSpellCostReduction())) >= stats.CurrentMana || 
+                state != SpellState.STATE_READY)
+                return false;
+            if(ManaCostsEnabled)
+                stats.CurrentMana = stats.CurrentMana - SpellData.ManaCost[Level] * (1 - stats.getSpellCostReduction());
+            X = x;
+            Y = y;
+            X2 = x2;
+            Y2 = y2;
+            Target = u;
+            FutureProjNetId = _networkIdManager.GetNewNetID();
+            SpellNetId = _networkIdManager.GetNewNetID();
+
+            if (SpellData.TargettingType == 1 && Target != null && Target.GetDistanceTo(Owner) > SpellData.CastRange[Level])
+            {
+                return false;
+            }
+
+            spellGameScript.OnStartCasting(Owner, this, Target);
+
+            if (SpellData.GetCastTime() > 0 && (SpellData.Flags & (int)SpellFlag.SPELL_FLAG_InstantCast) == 0)
+            {
+                Owner.setPosition(Owner.X, Owner.Y);//stop moving serverside too. TODO: check for each spell if they stop movement or not
+                state = SpellState.STATE_CASTING;
+                CurrentCastTime = SpellData.GetCastTime();
+            }
+            else
+            {
+                finishCasting();
+            }
+            var response = new CastSpellResponse(this, x, y, x2, y2, FutureProjNetId, SpellNetId);
+            _game.PacketHandlerManager.broadcastPacket(response, Channel.CHL_S2C);
+            return true;
+        }
+        
+        /// <summary>
+        /// Called when the spell is finished casting and we're supposed to do things such as projectile spawning, etc.
+        /// </summary>
+        public virtual void finishCasting()
+        {
+            spellGameScript.OnFinishCasting(Owner, this, Target);
+
+            if (SpellData.ChannelDuration[Level] == 0)
+            {
+                state = SpellState.STATE_COOLDOWN;
+
+                CurrentCooldown = getCooldown();
+
+                if (Slot < 4)
+                {
+                    _game.PacketNotifier.NotifySetCooldown(Owner, Slot, CurrentCooldown, getCooldown());
+                }
+
+                Owner.IsCastingSpell = false;
+            }
+        }
+
+        /// <summary>
+        /// Called when the spell is started casting and we're supposed to do things such as projectile spawning, etc.
+        /// </summary>
+        public virtual void channel()
+        {
+            state = SpellState.STATE_CHANNELING;
+            CurrentChannelDuration = SpellData.ChannelDuration[Level];
+        }
+
+        /// <summary>
+        /// Called when the character finished channeling
+        /// </summary>
+        public virtual void finishChanneling()
+        {
+            state = SpellState.STATE_COOLDOWN;
+
+            CurrentCooldown = getCooldown();
+
+            if (Slot < 4)
+            {
+                _game.PacketNotifier.NotifySetCooldown(Owner, Slot, CurrentCooldown, getCooldown());
+            }
+
+            Owner.IsCastingSpell = false;
+        }
+
+        /// <summary>
+        /// Called every diff milliseconds to update the spell
+        /// </summary>
+        public virtual void update(float diff)
+        {
+            spellGameScript.OnUpdate(diff);
+            switch (state)
+            {
+                case SpellState.STATE_READY:
+                    break;
+                case SpellState.STATE_CASTING:
+                    Owner.IsCastingSpell = true;
+                    CurrentCastTime -= diff / 1000.0f;
+                    if (CurrentCastTime <= 0)
+                    {
+                        finishCasting();
+                        if(SpellData.ChannelDuration[Level] > 0)
+                        {
+                            channel();
+                        }
+                    }
+                    break;
+                case SpellState.STATE_COOLDOWN:
+                    CurrentCooldown -= diff / 1000.0f;
+                    if (CurrentCooldown < 0)
+                    {
+                        state = SpellState.STATE_READY;
+                    }
+                    break;
+                case SpellState.STATE_CHANNELING:
+                    CurrentChannelDuration -= diff / 1000.0f;
+                    if(CurrentChannelDuration <= 0)
+                    {
+                        finishChanneling();
+                    }
+                    break;
+            }
+        }
+
+        /// <summary>
+        /// Called by projectiles when they land / hit, this is where we apply damage/slows etc.
+        /// </summary>
+        public void applyEffects(AttackableUnit u, Projectile p = null)
+        {
+            if (SpellData.HaveHitEffect && !string.IsNullOrEmpty(SpellData.HitEffectName))
+            {
+                ApiFunctionManager.AddParticleTarget(Owner, SpellData.HitEffectName, u);
+            }
+
+            spellGameScript.ApplyEffects(Owner, u, this, p);
+            ApiEventManager.OnSpellHit.Publish(u, this);
+        }
+
+        public void AddProjectile(string nameMissile, float toX, float toY, bool isServerOnly = false)
+        {
+            var p = new Projectile(
+                Owner.X,
+                Owner.Y,
+                (int) SpellData.LineWidth,
+                Owner,
+                new Target(toX, toY),
+                this,
+                SpellData.MissileSpeed,
+                nameMissile,
+                SpellData.Flags
+            );
+            _game.ObjectManager.AddObject(p);
+            if (!isServerOnly)
+            {
+                _game.PacketNotifier.NotifyProjectileSpawn(p);
+            }
+        }
+
+        public void AddProjectileTarget(string nameMissile, Target target, bool isServerOnly = false)
+        {
+            var p = new Projectile(
+                Owner.X,
+                Owner.Y,
+                (int)SpellData.LineWidth,
+                Owner,
+                target,
+                this,
+                SpellData.MissileSpeed,
+                nameMissile,
+                SpellData.Flags
+            );
+            _game.ObjectManager.AddObject(p);
+            if (!isServerOnly)
+            {
+                _game.PacketNotifier.NotifyProjectileSpawn(p);
+            }
+        }
+
+        public void AddLaser(float toX, float toY, bool affectAsCastIsOver = true)
+        {
+            var l = new Laser(
+                Owner.X,
+                Owner.Y,
+                (int)SpellData.LineWidth,
+                Owner,
+                new Target(toX, toY),
+                this,
+                SpellData.Flags,
+                affectAsCastIsOver
+            );
+            _game.ObjectManager.AddObject(l);
+        }
+
+        public void spellAnimation(string animName, AttackableUnit target)
+        {
+            _game.PacketNotifier.NotifySpellAnimation(target, animName);
+        }
+
+        /// <returns>spell's unique ID</returns>
+        public int getId()
+        {
+            return (int)HashFunctions.HashString(SpellName);
+        }
+
+        public string getStringForSlot()
+        {
+            switch (Slot)
+            {
+                case 0:
+                    return "Q";
+                case 1:
+                    return "W";
+                case 2:
+                    return "E";
+                case 3:
+                    return "R";
+                case 14:
+                    return "Passive";
+            }
+
+            return "undefined";
+        }
+
+        /**
+         * TODO : Add in CDR % from champion's stat
+         */
+        public float getCooldown()
+        {
+            if (CooldownsEnabled)
+                return SpellData.Cooldown[Level];
+            else
+                return 0f;
+        }
+
+        public virtual void levelUp()
+        {
+            if (Level <= 5)
+            {
+                ++Level;
+            }
+            if (Slot < 4)
+            {
+                Owner.GetStats().ManaCost[Slot] = SpellData.ManaCost[Level];
+            }
+            ApiEventManager.OnLevelUpSpell.Publish(this, Owner);
+        }
+
+        public void SetCooldown(byte slot, float newCd)
+        {
+            var targetSpell = Owner.Spells[slot];
+
+            if (newCd <= 0)
+            {
+                _game.PacketNotifier.NotifySetCooldown(Owner, slot, 0, 0);
+                targetSpell.state = SpellState.STATE_READY;
+                targetSpell.CurrentCooldown = 0;
+            }
+            else
+            {
+                _game.PacketNotifier.NotifySetCooldown(Owner, slot, newCd, targetSpell.getCooldown());
+                targetSpell.state = SpellState.STATE_COOLDOWN;
+                targetSpell.CurrentCooldown = newCd;
+            }
+        }
+
+        public void LowerCooldown(byte slot, float lowerValue)
+        {
+            SetCooldown(slot, Owner.Spells[slot].CurrentCooldown - lowerValue);
+        }
+    }
+}
